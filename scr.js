6ac0b3364b6d4910d3fe0cd3f45a8f835d742918
layzveb <layzveb@yandex.ru>
2020-06-14 23:21:12 +0300

    Update scr.js

--- scr.js
+++ scr.js
@@ -1,8 +1,18 @@
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-    /*Element.requestFullscreen();*/
+let detect = new MobileDetect(window.navigator.userAgent);
+if (MobileDetect == /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/) {
+    Element.requestFullscreen();  
     document.getElementById("style").href="CSS/designForMob.css";
     alert("это телефон");
 
 } else {
     document.getElementById("style").href="CSS/designForPC.css";
 }
+
+/*if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    /*Element.requestFullscreen();   
+    document.getElementById("style").href="CSS/designForMob.css";
+    alert("это телефон");
+
+} else {
+    document.getElementById("style").href="CSS/designForPC.css";
+}*/
