7cb6b7645fe0f806724d585bcb5a8a7bbc28efe5
layzveb <layzveb@yandex.ru>
2020-06-14 23:12:07 +0300

    2.2

--- scr.js
+++ scr.js
@@ -1,9 +1,8 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-    Element.requestFullscreen();
+    /*Element.requestFullscreen();*/
     document.getElementById("style").href="CSS/designForMob.css";
     alert("это телефон");
 
 } else {
-    alert("это PC");
     document.getElementById("style").href="CSS/designForPC.css";
 }
