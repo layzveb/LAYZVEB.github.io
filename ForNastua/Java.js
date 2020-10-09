1d40d69339ef658b9443a2036bbf20d2b5cb6020
layzveb <51766452+layzveb@users.noreply.github.com>
2020-10-10 00:42:03 +0300

    x

--- ForNastua/Java.js
+++ ForNastua/Java.js
@@ -1,5 +1,5 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-  document.getElementById('Styles').href='For Mobile.css';
+  document.getElementById('Styles').href='CSS/For Mobile.css';
   } else {
-    
+
 }
