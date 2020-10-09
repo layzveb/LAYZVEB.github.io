a1d4557ebc8feb2e2fa88d8e017c58677a32984b
layzveb <51766452+layzveb@users.noreply.github.com>
2020-10-09 23:04:21 +0300

    Reclama

---
+++ ForNastua/Java.js
@@ -0,0 +1,5 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    // код для мобильных устройств
+  } else {
+    document.getElementById('Styles').href='For Mobile.css';
+}
